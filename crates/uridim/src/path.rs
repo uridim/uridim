0a23bc6539676f12c2a837e124c24119256026f5
Curnic Dorin <157069618+C-Dorin@users.noreply.github.com>
2026-07-06 23:16:24 +0300

    feat(path): navigate parent directories

---
+++ crates/uridim/src/path.rs
@@ -0,0 +1,11 @@
+use std::path::PathBuf;
+
+pub fn print_paths() -> std::io::Result<()> {
+  let current_directory: PathBuf = std::env::current_dir()?;
+
+  for directory in current_directory.ancestors() {
+    println!("{}", directory.display());
+  }
+
+  Ok(())
+}
